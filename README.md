305528ea4e974b9da88513ea9458a85ebb4d0677
EliCash82 <AlmostAnderson@gmail.com>
2014-05-06 20:19:39 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Service_Design_Ruby
+
+**WARNING:** This is not a fully functional application, this is for
+testing purposes.
+
+I am attempting a project that will allow me to have a stronger handle over
+Object Relational Mapping from DataMapper/ActiveRecord Ruby to an SQL relational
+database.
+
+| Using:        | Implemented |
+| ------------- | ----------- |
+| Sinatra       | y           |
+| Active Record | y           |
+| RSpec         | y           |
+
+---
+
+*Bibliography of sources:*
+
+version 0.0.1: The Oh So Not Proper Dirty Rough Draft
+
+(ain't no style guiding here - Sorry MLA...)
+
+| Source                                    | Metadata       |
+| ----------------------------------------- | -------------- |
+| data-mapper.org/getting-started           | site           |
+| Service Oriented Design in Ruby and Rails | book, Paul Dix |
+| wikipedia.org/Object-relational_mapping   | site, wp       |
+
+*Interim License*
+
+[Attribution-NonCommercial-ShareAlike 4.0 International (CC BY-NC-SA 4.0)](http://creativecommons.org/licenses/by-nc-sa/4.0/)
---
+++ spec/service_spec.rb
@@ -0,0 +1,61 @@
+require File.dirname(__FILE__) + '/../service'
+require 'spec'
+require 'spec/interop/test'
+require 'rack/test'
+
+set :environment, :test
+Test::Unit::TestCase.send :include, Rack::Test::Methods
+
+def app
+  Sinatra::Application
+end
+
+describe "service" do
+  before(:each) do
+    User.delete_all
+  end
+
+  describe "GET on /api/v1/users/:id" do
+    before(:each) do
+      User.create(
+      :name => "anderson",
+      :email => "almostanderson@gmail.com",
+      :password => "strongpass",
+      :bio => "rubyist"
+      )
+    end
+
+    it "should return a user by name" do
+      get '/api/v1/users/anderson'
+      last_response.should be_ok
+      attributes = JSON.parse(last_response.body)
+      attributes["name"].should == "anderson"
+    end
+
+    it "should return a user with an email" do
+      get '/api/v1/users/anderson'
+      last_response.should be_ok
+      attributes = JSON.parse(last_response.body)
+      attributes["email"].should == "almostanderson@gmail.com"
+    end
+
+    it "should not return a user's password" do
+      get '/api/v1/users/anderson'
+      last_response.should be_ok
+      attributes = JSON.parse(last_response.body)
+      attributes/should_not have_key("password")
+    end
+
+    it "should return a user with a bio" do
+      get '/api/v1/users/anderson'
+      last_response.should be_ok
+      attributes = JSON.parse(last_response.body)
+      attributes["bio"].should == "rubyist"
+    end
+
+    it "should return a 404 for a user that doesn't exist" do
+      get '/api/v1/users/foo'
+      last_response.status.should == 404
+    end
+  end
+end
